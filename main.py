4db11f7f43c238f5dc1c13c752d1426cc26083a8
bernas-ernesto <thonbernas1134@gmail.com>
2025-05-13 23:37:52 +0800

    Import Base Address from Bernas Module

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-#TODO Bernas - Fake Address Module 
+#TODO Bernas - Fake Address Module
+from bernas import base_address
 
 #TODO Buenacifra - Username Module
 
 #TODO Roldan - Superhero Name Module
 
 #TODO Tero - Costume Color Module
+
+print(f"Base Address: {base_address}")
