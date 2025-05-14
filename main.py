1a74727f513c86d1154a6fc121ce61344d31cd55
altheno <ishigamisenku8888@gmail.com>
2025-05-14 23:45:43 +0800

    edit tero.py & main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from bernas import base_address
 from buenacifra import username
 from roldan import hero_name
-from tero import random_food
+from tero import get_random_food
 
 print("\nSuperhero Information Generator\n")
 print(f"Hero Name: {hero_name}")
 print(f"Username: {username}")
 print(f"Base Address: {base_address}")
-print(f"Favorite Food: {random_food}")
+print(f"Favorite Food: {get_random_food()}")
--- tero.py
+++ tero.py
@@ -13,5 +13,3 @@ def get_emoji_menu():
 def get_random_food():
     food_items = get_emoji_menu().strip().split("\n")
     return random.choice(food_items)
-
-random_food = get_random_food()
