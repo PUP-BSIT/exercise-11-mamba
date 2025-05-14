8175fd4e091e9ce656ed579caa04565fa79a5c04
buenacifra-abrianne <abriannebuenacifra@gmail.com>
2025-05-14 23:38:45 +0800

    add function in buenacifra module, call in main

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from bernas import base_address
-from buenacifra import username
+from buenacifra import print_username
 from roldan import hero_name
 from tero import random_food
 
 print("\nSuperhero Information Generator\n")
 print(f"Hero Name: {hero_name}")
-print(f"Username: {username}")
+print_username()
 print(f"Base Address: {base_address}")
 print(f"Favorite Food: {random_food}")
